1b1f904eb2ac4bafde7ab1ae59ad79f2950616d6
Ramirez Vargas, José Pablo <webJose@gmail.com>
2025-02-21 21:00:34 -0600

    docs(JsDoc):  Correct typo in singleSpaSvelte()

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -8,7 +8,7 @@ import singleSpaSvelteFactory from './single-spa/single-spa.svelte.js';
  * Creates single-spa lifecycle functions for a Svelte v5 component.
  * @param component Svelte component to mount as a single-spa micro-frontend or parcel.
  * @param domElementGetter Optional function that returns the DOM element where the component is mounted.
- * @param options Optional set of options, including Svelve mounting options.
+ * @param options Optional set of options, including Svelte mounting options.
  * @returns An object containing the single-spa lifecycle functions for the component.
 */
 export const singleSpaSvelte = singleSpaSvelteFactory();
